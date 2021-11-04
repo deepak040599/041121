61a8a0b7919bc5ef5b8c1e27c4ffe4cfa7d69714
Deepak Suthar <deepak040599@gmail.com>
2021-11-04 11:23:09 +0530

    22

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',()=>{
+    let long;
+    let lat;
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position=>{
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+        });
+    }
+});
